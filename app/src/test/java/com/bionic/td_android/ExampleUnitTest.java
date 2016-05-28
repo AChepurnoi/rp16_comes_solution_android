94ef53f70b15601e02c773eb514d492393cabe78
graniumHub <granium90@gmail.com>
2016-05-28 17:20:59 +0300

    UI update

--- app/src/test/java/com/bionic/td_android/ExampleUnitTest.java
+++ app/src/test/java/com/bionic/td_android/ExampleUnitTest.java
@@ -2,7 +2,6 @@ package com.bionic.td_android;
 
 import android.content.res.AssetManager;
 
-import com.bionic.td_android.MainWindow.SugarApp;
 
 import org.junit.Test;
 
@@ -14,15 +13,5 @@ import static org.junit.Assert.assertEquals;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
 
-    @Test
-    public void openFile() throws Exception {
-        AssetManager am = SugarApp.getAppContext().getAssets();
-        InputStream is = am.open("test.txt");
-        System.out.println(is);
-    }
 }
